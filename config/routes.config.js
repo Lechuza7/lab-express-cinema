19f48e7e92a87b0bcd03edc70a23f8ba294523a1
Marc Cuesta <marc.cuestacarabasa@gmail.com>
2022-07-02 15:03:07 +0200

    Iterations 1-2 done, iteration 3 started

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,7 +1,11 @@
 const express = require('express');
-
 const router = express.Router();
 
+const cinema = require("../controllers/cinema.controller");
+
 // Routes here
+
+router.get("/", cinema.index);
+router.get("/movies", cinema.movies);
 
 module.exports = router;
